f64cd028851eaaaf176c05fff2327824da15f76d
nuuco <nuuco.coding@gmail.com>
2022-08-04 03:12:47 +0900

    feat: 마크업 수정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Light from './Light';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>TDD : test 파일 만들어보기</header>
+      <main>
+        <Light name="전원" />
+      </main>
     </div>
   );
 }
